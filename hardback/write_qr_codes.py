9eeda2f1c3a82ef38039d6b2053f9519f849a6a5
Tom Ritchford <tom@swirly.com>
2019-04-03 19:52:00 +0200

    Add a nifty progress bar

--- hardback/write_qr_codes.py
+++ hardback/write_qr_codes.py
@@ -1,31 +1,59 @@
-import itertools, json, struct
+import datetime, itertools, json, math, struct
 from . import hasher, header, qr
+from progress.bar import ChargingBar
 
 PARENT_SIZE = 16
 BLOCK_SIZE = 1024
-SEQNO_SIZE = 8
-CHUNK_SIZE = PARENT_SIZE + BLOCK_SIZE + SEQNO_SIZE
-
+SEQUENCE_NUMBER_SIZE = 8
+CHUNK_SIZE = PARENT_SIZE + BLOCK_SIZE + SEQUENCE_NUMBER_SIZE
 assert CHUNK_SIZE <= qr.CHUNK_SIZE
+
+
+class ElapsedBar(ChargingBar):
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+        self.start_time = datetime.datetime.now()
+        self.index = 0
+
+    def next_item(self, message):
+        self.index += 1
+        elapsed = datetime.datetime.now() - self.start_time
+        time_per_item = elapsed / self.index
+        remaining_items = 1 + (self.max - self.index)
+        remaining = time_per_item * remaining_items
+
+        def fmt(t):
+            return str(t).split('.', 1)[0]
+
+        self.message = '%s %s elapsed, %s to go' % (
+            message, fmt(elapsed), fmt(remaining))
+
+        self.next()
 
 
 def write_qr_codes(filename, outfile):
     metadata = header.header(filename)
-    parent = bytes.fromhex(metadata['sha256'][:PARENT_SIZE])
+    parent = bytes.fromhex(metadata['sha256'])[:PARENT_SIZE]
     metadata_blocks = (json.dumps(metadata).encode(),)
     file_blocks = hasher.file_blocks(filename, BLOCK_SIZE)
     blocks = itertools.chain(metadata_blocks, file_blocks)
+    file_blocks, rem = divmod(metadata['size'], BLOCK_SIZE)
+    file_blocks += 1 + bool(rem)
+    blocks_digits = math.ceil(math.log(file_blocks, 16))
+    file_format = f'{outfile}-%0{blocks_digits}x'
 
-    for seqno, block in enumerate(blocks):
-        fmt = b'>q%ds%ds' % (PARENT_SIZE, len(block))
-        chunk = struct.pack(fmt, seqno, parent, block)
+    bar = ElapsedBar('Writing files', max=file_blocks)
+
+    for sequence_number, block in enumerate(blocks):
+        chunk = struct.pack(f'>q', sequence_number) + parent + block
         assert len(chunk) <= CHUNK_SIZE
+        filename = file_format % sequence_number
+        bar.next_item(qr.write(chunk, filename))
 
-        filename = '%s-%016x' % (outfile, seqno)
-        yield qr.write(chunk, filename)
+    bar.finish()
 
 
 if __name__ == '__main__':
     import sys
-    for f in write_qr_codes(*sys.argv[1:]):
-        print('Written', f)
+    for i, f in enumerate(write_qr_codes(*sys.argv[1:])):
+        pass
