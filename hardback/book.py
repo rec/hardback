f3c7fe1a0553756053941423491b6e152cdc1e66
Tom Ritchford <tom@swirly.com>
2019-04-04 20:58:05 +0200

    First start at book.py

---
+++ hardback/book.py
@@ -0,0 +1,27 @@
+from attr import dataclass
+
+
+@dataclass
+class DublinCore:
+    contributor: str = ''
+    coverage: str = ''
+    creator: str = ''
+    date: str = ''
+    description: str = ''
+    format: str = ''
+    identifier: str = ''
+    language: str = ''
+    publisher: str = ''
+    relation: str = ''
+    rights: str = ''
+    source: str = ''
+    subject: str = ''
+    title: str = ''
+    type: str = ''
+
+
+@dataclass
+class Book:
+    identifier: str = ''
+    title: str = ''
+    language: str = 'en'
