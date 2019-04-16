e19a05d7c4454778916cf95335f353dc38ccbd1a
Tom Ritchford <tom@swirly.com>
2019-04-16 20:50:36 +0200

    Split cover.py off dataclass.py

--- hardback/dataclass.py
+++ hardback/dataclass.py
@@ -1,9 +1,7 @@
 """Contains data classes that specify how a book is put together"""
 
 from .qr.writer import Writer
-from PIL import ImageFont
 from attr import dataclass, Factory
-from pathlib import Path
 from typing import List
 
 
@@ -35,32 +33,3 @@ class Hardback:
     progress_bar: bool = True
     remove_image_files: bool = True
     qr_image_dir: str = '.output'
-
-
-@dataclass
-class Font:
-    name: str = '/Library/Fonts/Courier New Bold.ttf'
-    size: int = 0
-
-    def make_image_font(self):
-        suffix = Path(self.name).suffix
-        if suffix == '.tff':
-            if not self.size:
-                raise ValueError(f'Size needs to be set for Truetype fonts')
-            return ImageFont.truetype(self.name, self.size)
-
-        if suffix == '.pil':
-            if self.size:
-                raise ValueError(f'Size cannot be set for Bitmap fonts')
-            return ImageFont.load(self.name)
-
-        raise ValueError(f'Do not understand file {self.file}')
-
-
-@dataclass
-class Cover:
-    title: str = ''
-    font: Font = Factory(Font)
-    margin: List[int] = Factory(lambda: [75, 50])
-    dimensions: List[int] = Factory(lambda: [2560, 1600])
-    # https://blog.reedsy.com/book-cover-dimensions/
