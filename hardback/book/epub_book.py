daf72be291557271fcd20ef2cda2c966717fe08e
Tom Ritchford <tom@swirly.com>
2019-04-22 12:36:15 +0200

    Extract epub initialization to dataclass.Book

--- hardback/book/epub_book.py
+++ hardback/book/epub_book.py
@@ -1,22 +1,10 @@
-from pathlib import Path
 from ebooklib import epub
 from . css import make_css
 
 
 class EpubBook(epub.EpubBook):
     def add_desc(self, book):
-        self.set_identifier(book.identifier)
-        self.set_title(book.title)
-        self.set_language(book.language)
-
-        for a in book.authors:
-            self.add_author(a)
-
-        if book.cover:
-            with open(book.cover, 'rb') as fp:
-                filename = 'cover_' + Path(book.cover).name
-                self.set_cover(filename, fp.read())
-
+        book.apply(self)
         self.default_css = make_css('default')
         self.add_item(self.default_css)
 
--- hardback/data/dataclass.py
+++ hardback/data/dataclass.py
@@ -2,6 +2,7 @@
 
 from .. qr.qr import QR
 from attr import dataclass, Factory
+from pathlib import Path
 from typing import List
 
 
@@ -19,6 +20,19 @@ class Book:
     cover: str = ''
     language: str = 'en'
     authors: List[str] = Factory(list)
+
+    def apply(self, ebook):
+        ebook.set_identifier(self.identifier)
+        ebook.set_title(self.title)
+        ebook.set_language(self.language)
+
+        for a in self.authors:
+            ebook.add_author(a)
+
+        if self.cover:
+            with open(self.cover, 'rb') as fp:
+                filename = 'cover_' + Path(self.cover).name
+                ebook.set_cover(filename, fp.read())
 
 
 @dataclass(slots=True)
